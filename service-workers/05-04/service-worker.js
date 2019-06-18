56002d81987b23b145a2145a1b1878ffa26b03e0
MagicPatje <pdenatris@live.nl>
2019-06-18 13:20:23 +0200

    Update service-worker.js

--- service-workers/05-04/service-worker.js
+++ service-workers/05-04/service-worker.js
@@ -12,7 +12,8 @@ self.addEventListener('install', function(event) {
     caches.open(currentCache.offline).then(function(cache) {
       return cache.addAll([
         offlineUrl,
-        'script.js'
+        'script.js',
+        'style2.css'
         // Here you can put all the files you need to be cached. Formatted: 'offline.html', 'test.js', 'css/style.css'
       ]);
     })
